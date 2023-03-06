186fa62c0c704b466a0c897908f85800dde79dbe
awesomelewis2007 <awesomelewis2007@gmail.com>
2023-03-06 20:12:15 +0000

    Make mv more robust

--- src/mv.cpp
+++ src/mv.cpp
@@ -12,6 +12,22 @@
 int main (int argc, char *argv[]) {
     std::string path = argc > 1 ? argv[1] : ".";
     std::string path2 = argc > 2 ? argv[2] : ".";
-    std::filesystem::rename(path, path2);
+    if (!std::filesystem::exists(path)) {
+        std::cerr << "Can not move " << path << std::endl;
+        std::cerr << "File does not exist" << std::endl;
+        return 1;
+    }
+    if (std::filesystem::exists(path2)) {
+        std::cerr << "Can not move " << path << " to " << path2 << std::endl;
+        std::cerr << "File already exists" << std::endl;
+        return 1;
+    }
+    try {
+        std::filesystem::rename(path, path2);
+    } catch (std::filesystem::filesystem_error &e) {
+        std::cerr << "Can not move " << path << " to " << path2 << std::endl;
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
