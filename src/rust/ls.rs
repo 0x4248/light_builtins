f4b70cb9c0ecf152589acedd3f4b9f4fa307ab8c
awesomelewis2007 <awesomelewis2007@gmail.com>
2023-03-19 07:48:48 +0000

    rust ls: add colouring to ls command

--- src/rust/ls.rs
+++ src/rust/ls.rs
@@ -8,6 +8,19 @@
 use std::env;
 use std::fs;
 use std::fs::metadata;
+use std::os::unix::prelude::PermissionsExt;
+
+pub mod colour {
+    pub const RED: &str = "\x1b[31m";
+    pub const GREEN: &str = "\x1b[32m";
+    pub const YELLOW: &str = "\x1b[33m";
+    pub const BLUE: &str = "\x1b[34m";
+    pub const MAGENTA: &str = "\x1b[35m";
+    pub const CYAN: &str = "\x1b[36m";
+    pub const WHITE: &str = "\x1b[37m";
+    pub const RESET: &str = "\x1b[0m";
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -24,9 +37,19 @@ fn main() {
         let file = path.to_str().unwrap();
         let file = &file.replace("./", "");
         if meta.is_dir() {
-            print!("/{} ", file);
+            print!("/{}{}{} ", colour::BLUE, file, colour::RESET);
         } else {
-            print!("{} ", file);
+            let perms = meta.permissions();
+            let mode = perms.mode();
+            if mode & 0o111 != 0 {
+                print!("{}{}{} ", colour::GREEN, file, colour::RESET);
+            }
+            else if mode & 0o444 != 0 {
+                print!("{}{}{} ", colour::RED, file, colour::RESET);
+            } else {
+                print!("{} ", file);
+            }
+
         }
     }
     println!("");
