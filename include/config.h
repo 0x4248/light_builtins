79117520420ccc85990542a19189745f7fc7414a
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-19 21:09:02 +0100

    Update comments in config.h
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- include/config.h
+++ include/config.h
@@ -2,6 +2,7 @@
  * A lightweight repository of useful shell commands
  * GitHub: https://www.github.com/awesomelewis2007/light_builtins
  * License: GNU General Public License v3.0
+ * By: Lewis Evans
  * 
  * This file contains the configuration for the Light builtins project.
  * This is so that all the c and c++ can access things like the version number.
@@ -10,6 +11,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/* Version number configuration */
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 2
 #define VERSION_PATCH 2
