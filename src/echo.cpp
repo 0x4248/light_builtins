b150184a62dc7cb5d7ef2edb9071861ec60147af
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-06-15 18:34:12 +0100

    Add comments to echo command
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- src/echo.cpp
+++ src/echo.cpp
@@ -12,14 +12,22 @@
 #include "config.h"
 
 int main(int argc, char *argv[]) {
+    /* Check if the user has provided the -v or --version flag to show the version */
     if (argc >= 2 &&
         (std::string(argv[1]) == "-v" || std::string(argv[1]) == "--version")) {
+        /* Print the version information */
         std::cout << "Light Builtins (C++) " << VERSION_MAJOR << "."
                   << VERSION_MINOR << "." << VERSION_PATCH << "-"
                   << EXTRA_VERSION << std::endl;
         return 0;
     }
-    std::string str = argc > 1 ? argv[1] : "";
-    std::cout << str << std::endl;
+    /* Get the current time */
+    std::time_t now = std::time(nullptr);
+    char date[100];
+    /* Format the time string */
+    std::strftime(date, sizeof(date), "%a %b %d %H:%M:%S %Z %Y",
+                  std::localtime(&now));
+    /* Print the time string */
+    std::cout << date << std::endl;
     return 0;
 }
