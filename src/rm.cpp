e60eb612ee269448378090f61157ab085dbd9a99
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-06-15 18:41:32 +0100

    Add comments to rm command
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- src/rm.cpp
+++ src/rm.cpp
@@ -14,14 +14,17 @@
 #include "config.h"
 
 int main(int argc, char *argv[]) {
+    /* Check if the user has provided the -v or --version flag to show the version */
     if (argc >= 2 &&
         (std::string(argv[1]) == "-v" || std::string(argv[1]) == "--version")) {
+        /* Print the version information */
         std::cout << "Light Builtins (C++) " << VERSION_MAJOR << "."
                   << VERSION_MINOR << "." << VERSION_PATCH << "-"
                   << EXTRA_VERSION << std::endl;
         return 0;
     }
     std::string path = argc > 1 ? argv[1] : ".";
+    /* Check if the path is a directory */
     if (std::filesystem::is_directory(path)) {
         /**
          * If the directory already exists, then we can not create it.
@@ -30,9 +33,12 @@ int main(int argc, char *argv[]) {
         std::cerr << "rm: can not remove directory " << std::endl;
         return 1;
     }
+    /* Check if the path exists */
     if (std::filesystem::exists(path)) {
+        /* Remove the file */
         std::filesystem::remove(path);
     } else {
+        /* Print an error message if the path does not exist */
         std::cerr << "rm: can not remove " << path
                   << " no such file or directory" << std::endl;
         return 1;
