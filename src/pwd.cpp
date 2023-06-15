60b2e219deac7ee060a278f26ce341b3bd93cc3a
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-06-15 18:40:37 +0100

    Add comments to pwd command
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- src/pwd.cpp
+++ src/pwd.cpp
@@ -13,8 +13,10 @@
 #include "config.h"
 
 int main(int argc, char *argv[]) {
+    /* Check if the user has provided the -v or --version flag to show the version */
     if (argc >= 2 &&
         (std::string(argv[1]) == "-v" || std::string(argv[1]) == "--version")) {
+        /* Print the version information */
         std::cout << "Light Builtins (C++) " << VERSION_MAJOR << "."
                   << VERSION_MINOR << "." << VERSION_PATCH << "-"
                   << EXTRA_VERSION << std::endl;
@@ -22,6 +24,7 @@ int main(int argc, char *argv[]) {
     }
     std::string path = std::filesystem::current_path();
     path.erase(remove(path.begin(), path.end(), '"'), path.end());
+    /* Print the current working directory to the console */
     std::cout << path << std::endl;
     return 0;
 }
