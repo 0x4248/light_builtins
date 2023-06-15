274e39555949438245e1a60582e74134c581ad98
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-06-15 18:46:49 +0100

    Fix issue on echo command
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- src/echo.cpp
+++ src/echo.cpp
@@ -16,19 +16,13 @@ int main(int argc, char *argv[]) {
      * version */
     if (argc >= 2 &&
         (std::string(argv[1]) == "-v" || std::string(argv[1]) == "--version")) {
-        /* Print the version information */
         std::cout << "Light Builtins (C++) " << VERSION_MAJOR << "."
                   << VERSION_MINOR << "." << VERSION_PATCH << "-"
                   << EXTRA_VERSION << std::endl;
         return 0;
     }
-    /* Get the current time */
-    std::time_t now = std::time(nullptr);
-    char date[100];
-    /* Format the time string */
-    std::strftime(date, sizeof(date), "%a %b %d %H:%M:%S %Z %Y",
-                  std::localtime(&now));
-    /* Print the time string */
-    std::cout << date << std::endl;
+    /* Print the arguments */
+    std::string str = argc > 1 ? argv[1] : "";
+    std::cout << str << std::endl;
     return 0;
 }
