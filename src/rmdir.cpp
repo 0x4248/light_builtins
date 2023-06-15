bf403861f39550998768a39dba0c84c188380ecf
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-06-15 18:42:28 +0100

    Add comments to rmdir command
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- src/rmdir.cpp
+++ src/rmdir.cpp
@@ -14,28 +14,39 @@
 #include "config.h"
 
 int main(int argc, char *argv[]) {
+    /* Check if the user has provided the -v or --version flag to show the version */
     if (argc >= 2 &&
         (std::string(argv[1]) == "-v" || std::string(argv[1]) == "--version")) {
+        /* Print the version information */
         std::cout << "Light Builtins (C++) " << VERSION_MAJOR << "."
                   << VERSION_MINOR << "." << VERSION_PATCH << "-"
                   << EXTRA_VERSION << std::endl;
         return 0;
     }
+    /* Check if the user has provided any arguments */
     if (argc == 1) {
+        /* Print the usage information */
         std::cout << "Usage: rmdir [-r] [path]" << std::endl;
         return 0;
     }
+    /* Check if the user has provided the -r flag to remove directories recursively */
     if (argc > 1 && std::string(argv[1]) == "-r") {
+        /* Get the path to the directory to remove */
         std::filesystem::path path = argc > 2 ? argv[2] : ".";
+        /* Remove the directory and its contents recursively */
         std::filesystem::remove_all(path);
         return 0;
     }
+    /* Get the path to the directory to remove */
     std::filesystem::path path = argc > 1 ? argv[1] : ".";
+    /* Check if the directory is empty */
     if (std::filesystem::is_empty(path)) {
+        /* Remove the directory */
         std::filesystem::remove(path);
     } else {
         /**
          * If the directory is not empty, then we can not remove it.
+         * We will print an error message using cerr and return 1.
          */
         std::cerr << "Directory is not empty" << std::endl;
         return 1;
