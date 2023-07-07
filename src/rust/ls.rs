99fed26c01ed33c083b942f7b4adce6678a52a45
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-07 11:23:57 +0100

    Add comments to ls command
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- src/rust/ls.rs
+++ src/rust/ls.rs
@@ -19,6 +19,7 @@ pub mod colour {
 }
 
 fn main() {
+    /* Get the command line arguments */
     let args: Vec<String> = env::args().collect();
     let path;
     if args.len() == 1 {
@@ -26,22 +27,29 @@ fn main() {
     } else {
         path = &args[1];
     }
+    /* List the directory contents */
     let paths = fs::read_dir(path).unwrap();
     for path in paths {
+        /* Get the file path */
         let path = path.unwrap().path();
         let meta = metadata(&path).unwrap();
         let file = path.to_str().unwrap();
         let file = &file.replace("./", "");
         if meta.is_dir() {
+            /* Print the directory name in blue */
             print!("/{}{}{} ", colour::BLUE, file, colour::RESET);
         } else {
+            /* Print the file name in green if it is executable, otherwise print it in red if it is read-only */
             let perms = meta.permissions();
             let mode = perms.mode();
             if mode & 0o111 != 0 {
+                /* The file is executable */
                 print!("{}{}{} ", colour::GREEN, file, colour::RESET);
             } else if mode & 0o444 != 0 {
+                /* The file is read-only */
                 print!("{}{}{} ", colour::RED, file, colour::RESET);
             } else {
+                /* The file is not executable or read-only */
                 print!("{} ", file);
             }
         }
