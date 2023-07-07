5a440873952448b26e3bd4cb2477234f46eb28c0
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-07 11:21:56 +0100

    Add comments to date
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- src/rust/date.rs
+++ src/rust/date.rs
@@ -9,7 +9,9 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
+    /* Get the current date and time */
     let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
     let date = chrono::Utc.timestamp(now as i64, 0).format("%a %b %d %H:%M:%S %Z %Y").to_string();
+    /* Print the date and time */
     println!("{}", date);
 }
