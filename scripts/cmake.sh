7a5559b07916b85d1d18a5811e65de3262544c26
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-12-22 16:51:41 +0000

    Add Cmake support
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

---
+++ scripts/cmake.sh
@@ -0,0 +1,11 @@
+# Light builtins's Makefile
+# A lightweight repository of useful shell commands
+# GitHub: https://www.github.com/lewisevans2007/light_builtins
+# Licence: GNU General Public License v3.0
+# By: Lewis Evans
+
+mkdir build
+cd build
+cmake ..
+cd ..
+make
